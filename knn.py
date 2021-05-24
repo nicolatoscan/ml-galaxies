84c8eff462a92ad69eaa2bb69fbd2de58d24feb9
nicolatoscan <nicolatoscan99@outlook.it>
2021-05-24 02:14:57 +0200

    knn

--- knn.py
+++ knn.py
@@ -12,7 +12,7 @@ from torch.utils.data import DataLoader
 from torchvision.transforms import Compose, RandomCrop, ToTensor, ColorJitter, Normalize, Resize
 from torchvision.datasets import ImageFolder
 
-from sklearn import ensemble
+from sklearn import neighbors
 import matplotlib.pyplot as plt
 import concurrent.futures
 
@@ -30,8 +30,9 @@ def load_dataset(path: str) -> ImageFolder:
         ToTensor(),
         # Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         # ColorJitter(0, 0.5, 0, 0),
-        lambda x: x.permute(1, 2, 0),
-        lambda x: x - X,
+        # lambda x: x.permute(1, 2, 0),
+        # lambda x: x - X,
+        # Resize((128, 128))
     ]) # remember to normalize the data
     dataset = ImageFolder(path, transform=transform)
     # idx_to_class = {v: k for k, v in dataset.class_to_idx.items()}
@@ -50,67 +51,47 @@ datasetValidate = load_dataset('./assets/dataset/validate')
 loaderTrain = load_images(datasetTrain, 256)
 loaderValidate = load_images(datasetValidate, 256)
 
-# %% Train
-randomForests = [
-    (ensemble.RandomForestClassifier(n_estimators=5, criterion=criterion), nr, criterion)
-    for nr in [1, 10, 20, 50, 100]
-    for criterion in ["gini", "entropy"]
-]
+
+# %% traim
+knn = neighbors.KNeighborsClassifier(n_jobs=-1)
 start = time.time()
 i = 0
 for images, labels in loaderTrain:
-    print(i)
-    i += 1
     images = cast(Tensor, images)
     images = torch.cat((
-            images,
-            # images.rot90(1, dims=[2, 3]),
-            # images.rot90(2, dims=[2, 3]),
-            # images.rot90(3, dims=[2, 3]),
-            # images.flip(dims=(2,)),
-            # images.flip(dims=(3,)),
-            # images.rot90(1, dims=[2, 3]).flip(dims=(2,)),
-            # images.rot90(1, dims=[2, 3]).flip(dims=(3,)),
-        ))
-    labels = labels.repeat(1)
-    images = images.reshape(images.size(0), -1)
-    with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-        iterator = executor.map(lambda t: t[0].fit(images, labels), randomForests)
-        list(iterator)
+        images,
+        images.rot90(1, dims=[2, 3]),
+        images.rot90(2, dims=[2, 3]),
+        images.rot90(3, dims=[2, 3]),
+        images.flip(dims=(2,)),
+        images.flip(dims=(3,)),
+        images.rot90(1, dims=[2, 3]).flip(dims=(2,)),
+        images.rot90(1, dims=[2, 3]).flip(dims=(3,)),
+    ))
+    images = images.mean(dim=3).reshape(images.size(0), -1)
+    labels = labels.repeat(8)
+    knn.fit(images, labels)
+    i += 1
+    print(i)
 end = time.time()
 print(f"Training time: {end-start}s")
 
 # %% Validate
 gtLabels = torch.tensor([]).int()
-pLabels = { }
-for crit in ["gini", "entropy"]:
-    pLabels[crit] = {}
-    for nr in [1, 10, 20, 50, 100]:
-        pLabels[crit][nr] = torch.tensor([]).int()
-i = 0
+pLabels = torch.tensor([]).int()
+
 start = time.time()
+i = 0
 for images, labels in loaderValidate:
-    print(i)
-    i += 1
-    images = cast(Tensor, images)
-    images = images.reshape(images.size(0), -1)
-    for tree, ms, crit in randomForests:
-        predictedLabels = torch.from_numpy(tree.predict(images))
-        pLabels[crit][ms] = torch.cat((pLabels[crit][ms], predictedLabels), 0)
+    images = cast(Tensor, images).mean(dim=3).reshape(images.size(0), -1)
+    predictedLabels = torch.from_numpy(knn.predict(images))
+    pLabels = torch.cat((pLabels, predictedLabels), 0)
     gtLabels = torch.cat((gtLabels, labels), 0)
+    i += 1
+    print(i)
 end = time.time()
-print(f"Evaluation time: {end-start}s")
+print(f"Validation time: {end-start}s")
 
 # %% Evaluate
-i = 0
-acc = 0
-for crit in pLabels:
-    for ms in pLabels[crit]:
-        i += 1
-        x = evaluate(gtLabels, pLabels[crit][ms])
-        print(f"{crit}\t{ms}\t{x['Acc']}")
-        acc += x['Acc']
-        # print(x['mAcc'])
-        # print(x['mIoU'])
-print(acc / i)
-# %%
+x = evaluate(gtLabels, pLabels)
+print(x)
