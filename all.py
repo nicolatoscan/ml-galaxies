f49998fbc4ead49b025c9c2932235eb9d2eeee47
nicolatoscan <nicolatoscan99@outlook.it>
2021-05-24 17:33:41 +0200

    train & predict all

--- all.py
+++ all.py
@@ -1,5 +1,6 @@
 # %% imports
 from typing import Any, Dict, List, Tuple
+from numpy.core.fromnumeric import trace
 
 import torch
 from torch.functional import Tensor
@@ -8,28 +9,36 @@ from torch.utils.data import DataLoader
 from torchvision.transforms import Compose, RandomCrop, ToTensor, ColorJitter
 from torchvision.datasets import ImageFolder
 
-from sklearn import tree
 import matplotlib.pyplot as plt
 import tqdm
 import concurrent.futures
 
 
 # %% Functions
-def load_dataset(path: str) -> ImageFolder:
-    transform = Compose([
-        RandomCrop(224),
-        ToTensor(),
-        # ColorJitter(0, 0.5, 0, 0),
-        lambda x: x.permute(1, 2, 0)
-    ]) # remember to normalize the data
+def load_dataset(path: str, isTrain:bool=True) -> ImageFolder:
+    transformations = []
+    if isTrain:
+        transformations = [
+            RandomCrop(224),
+            ToTensor(),
+            # ColorJitter(0, 0.5, 0, 0),
+            lambda x: x.permute(1, 2, 0)
+        ]
+    else:
+        transformations = [
+            ToTensor(),
+            lambda x: x.permute(1, 2, 0)
+        ]
+
+    transform = Compose(transformations) # remember to normalize the data
     dataset = ImageFolder(path, transform=transform)
     # idx_to_class = {v: k for k, v in dataset.class_to_idx.items()}
     return dataset
 
-def load_images(dataset: ImageFolder, batch_size=128) -> DataLoader:
+def load_images(dataset: ImageFolder, batch_size=128, shuffle:bool=True) -> DataLoader:
     return DataLoader(  dataset,
                         batch_size=batch_size,
-                        shuffle=True,
+                        shuffle=shuffle,
                         num_workers=2,
                         pin_memory=True,
                         drop_last=True)
@@ -37,10 +46,10 @@ def load_images(dataset: ImageFolder, batch_size=128) -> DataLoader:
 def parseImageLabels(images: Tensor, labels: Tensor) -> Tuple[Tensor, Tensor]:
     return (images.reshape(images.size(0), -1), labels)
 
-def trian(models: List[Tuple[Any, str]]):
+def train(models: List[Tuple[Any, str]], max_workers: int = 8):
     for i, l in tqdm(loaderTrain):
         (images, labels) = parseImageLabels(i, l)
-        with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
             list(
                 executor.map(lambda m: m[0].fit(images, labels), models)
             )
@@ -51,23 +60,23 @@ def predictModel(model, images: Tensor, name: str, pLabels: Dict[str, Tensor]):
     predictedLabels = torch.from_numpy(model.predict(images))
     pLabels[name] = torch.cat((pLabels[name], predictedLabels), 0)
 
-def predict(models: List[Tuple[Any, str]]) -> Tuple[Tensor, Dict[str, Tensor]]:
+def predict(models: List[Tuple[Any, str]], max_workers: int = 8) -> Tuple[Tensor, Dict[str, Tensor]]:
     gtLabels = torch.tensor([]).int()
     pLabels: Dict[str, Tensor] = {}
-    for m, name in models:
+    for _, name in models:
         pLabels[name] = torch.tensor([]).int()
     for i, l in tqdm(loaderValidate):
         (images, labels) = parseImageLabels(i, l)
         gtLabels = torch.cat((gtLabels, labels), 0)
-        with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
             list(
                 executor.map(lambda m: predictModel(m[0], images, m[1], pLabels)
                 , models)
             )
     return (gtLabels, pLabels)
 
-def evaluate(yt: torch.Tensor, yp: torch.Tensor, num_classes=10) -> Dict[str, float]:
-    C=(yt*num_classes+yp).bincount(minlength=num_classes**2).view(num_classes,num_classes).float()
+def evaluate(gtLabels: torch.Tensor, pLabels: torch.Tensor, num_classes=10) -> Dict[str, float]:
+    C=(gtLabels*num_classes+pLabels).bincount(minlength=num_classes**2).view(num_classes,num_classes).float()
     return {
         'Acc': C.diag().sum().item() / yt.shape[0],
         'mAcc': (C.diag()/C.sum(-1)).mean().item(),
@@ -86,7 +95,35 @@ loaderTrain = load_images(datasetTrain, 256)
 loaderValidate = load_images(datasetValidate, 256)
 
 
-# %% train
+# %% Train
+from sklearn import tree, ensemble, neighbors, svm
 
+models: List[Tuple[Any, str]] = []
+# Decision Tree
+models += [
+    (tree.DecisionTreeClassifier(min_samples_leaf=1, criterion='gini'), f'dTree {criterion}{nr}')
+    for nr in [1, 5]
+    for criterion in ["gini", "entropy"]
+]
+# Forest
+models += [
+    (ensemble.RandomForestClassifier(n_estimators=1, criterion='gini'), f'Forest {criterion}{nr}')
+    for nr in [1, 5, 10, 20, 50, 100]
+    for criterion in ["gini", "entropy"]
+]
+# KNN
+models += [
+    (neighbors.KNeighborsClassifier(n_jobs=-1), 'KNN'),
+]
+#SVM
+models += [
+    (svm.SVC(kernel=kernel, C=C), f'SVM {kernel}{C}')
+    for kernel in ['rbf', 'linear', 'poly'] 
+    for C in [0.1, 1, 10]
+]
 
-# %%
+train(models)
+
+# %% Predict
+gtLabels, pLabels = predict(models)
+res = evaluateAll(gtLabels, pLabels)
